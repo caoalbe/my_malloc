631300090e17ba3391d852e68c4ffbbef40109ff
Albert Cao <albert.cao.1000@gmail.com>
2025-05-28 17:21:57 -0400

    malloc with sbrk

--- lib.c
+++ lib.c
@@ -1,7 +1,14 @@
 #include "lib.h"
+#include <stddef.h>
+#include <unistd.h>
 
 void* my_malloc(size_t size) {
+    if (size == 0) { return NULL; }
 
+    void* output = sbrk(size);
+    if (output == (void *) -1) { return NULL; } // sbrk has failed
+
+    return output;
 }
 
 void my_free(void* ptr) {
