b06b05624eee2b196e4c6817574e835fca40e664
Albert Cao <albert.cao.1000@gmail.com>
2025-05-28 18:49:54 -0400

    added meta_block to start of malloc

--- lib.c
+++ lib.c
@@ -2,13 +2,24 @@
 #include <stddef.h>
 #include <unistd.h>
 
+struct meta_block {
+    size_t size;
+    struct meta_block* next;
+    int vacant;
+};
+
 void* my_malloc(size_t size) {
     if (size == 0) { return NULL; }
 
-    void* output = sbrk(size);
+    void* output = sbrk(size + sizeof(struct meta_block));
     if (output == (void *) -1) { return NULL; } // sbrk has failed
 
-    return output;
+    struct meta_block *meta_pointer = (struct meta_block *)output;
+    meta_pointer->size = size;
+    meta_pointer->vacant = 99;
+    meta_pointer->next = NULL;
+
+    return output + sizeof(struct meta_block);
 }
 
 void my_free(void* ptr) {
