e8997b15e4607663dd497babfc3c4ad5dcc0931a
Albert Cao <albert.cao.1000@gmail.com>
2025-05-29 18:19:29 -0400

    replace sizeof(struct meta_block) with preprocessor

--- lib.c
+++ lib.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define META_BYTE_SIZE sizeof(struct meta_block)
 struct meta_block {
     size_t size;
     struct meta_block* next;
@@ -18,10 +19,10 @@ void *BLOCK_HEAD = NULL;
 void* write_meta_block(void* address, size_t size, struct meta_block* prev, struct meta_block* next, int vacant) {
     if (address == NULL) {
         // Write block at end of heap
-        address = sbrk(size + sizeof(struct meta_block));
+        address = sbrk(size + META_BYTE_SIZE);
         if (address == (void *) -1) { return NULL; } // sbrk has failed
     }
-    assert(address + sizeof(struct meta_block) + size <= sbrk(0)); // Ensures there is sufficient space to write
+    assert(address + META_BYTE_SIZE + size <= sbrk(0)); // Ensures there is sufficient space to write
 
     struct meta_block *meta_pointer = (struct meta_block *)address;
     meta_pointer->size = size;
@@ -31,7 +32,7 @@ void* write_meta_block(void* address, size_t size, struct meta_block* prev, stru
     if (prev != NULL) { prev->next = address; }
     if (next != NULL) { next->prev = address; }
 
-    return address + sizeof(struct meta_block);
+    return address + META_BYTE_SIZE;
 }
 
 void* my_malloc(size_t size) {
@@ -39,7 +40,7 @@ void* my_malloc(size_t size) {
     if (BLOCK_HEAD == NULL) {
         // Initialize linked list
         void* data_addr = write_meta_block(NULL, size, NULL, NULL, 0);
-        BLOCK_HEAD = data_addr - sizeof(struct meta_block);
+        BLOCK_HEAD = data_addr - META_BYTE_SIZE;
         return data_addr;
     }
 
@@ -49,15 +50,15 @@ void* my_malloc(size_t size) {
     while (curr != NULL) {
         if (curr->vacant == 1 && size <= curr->size) {
             curr->vacant = 0;
-            if (curr->size - size > sizeof(struct meta_block)) {
+            if (curr->size - size > META_BYTE_SIZE) {
                 // Truncate Block to Minimal Length
-                void* new_meta_block = (void*) curr + sizeof(struct meta_block) + size;
-                size_t new_size = curr->size - size - sizeof(struct meta_block);
+                void* new_meta_block = (void*) curr + META_BYTE_SIZE + size;
+                size_t new_size = curr->size - size - META_BYTE_SIZE;
                 write_meta_block(new_meta_block, new_size, curr, curr->next, 1);
                 curr->size = size;
             }
 
-            return sizeof(struct meta_block) + (void *)curr;
+            return META_BYTE_SIZE + (void *)curr;
         }
         prev = curr;
         curr = curr->next;
@@ -69,18 +70,18 @@ void* my_malloc(size_t size) {
 
 void my_free(void* ptr) {
     if (ptr == NULL) { return; }
-    struct meta_block *meta_pointer = (struct meta_block *)(ptr - sizeof(struct meta_block));
+    struct meta_block *meta_pointer = (struct meta_block *)(ptr - META_BYTE_SIZE);
     meta_pointer->vacant = 1;
 
     // merge with successor if its also vacant
     if (meta_pointer->next != NULL &&  meta_pointer->next->vacant == 1) {
-        meta_pointer->size = meta_pointer->size + sizeof(struct meta_block) + meta_pointer->next->size;
+        meta_pointer->size = meta_pointer->size + META_BYTE_SIZE + meta_pointer->next->size;
         meta_pointer->next = meta_pointer->next->next;
     }
 
     // merge with predecessor if its also vacant
     if (meta_pointer->prev != NULL && meta_pointer->prev->vacant == 1) {
-        meta_pointer->prev->size = meta_pointer->prev->size + sizeof(struct meta_block) + meta_pointer->size;
+        meta_pointer->prev->size = meta_pointer->prev->size + META_BYTE_SIZE + meta_pointer->size;
         meta_pointer->prev->next = meta_pointer->next;
     }
 }
