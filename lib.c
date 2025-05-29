be094db2ee08b8ea65ba043751002e90a2b9ffed
Albert Cao <albert.cao.1000@gmail.com>
2025-05-29 11:39:43 -0400

    can search for vacant block

--- lib.c
+++ lib.c
@@ -1,6 +1,7 @@
 #include "lib.h"
 #include <stddef.h>
 #include <unistd.h>
+#include <stdio.h>
 
 struct meta_block {
     size_t size;
@@ -10,10 +11,23 @@ struct meta_block {
 
 void *BLOCK_HEAD = NULL;
 
-void* my_malloc(size_t size) {
-    if (size == 0) { return NULL; }
-    if (BLOCK_HEAD == NULL) { BLOCK_HEAD = sbrk(0); }
+// Finds a block with sufficient size;
+// if such a block doesnt exist; then allocate the necessary space with sbrk
+void* find_block(size_t size) {
+    // Search linked list
+    struct meta_block* prev = NULL;
+    struct meta_block* curr = (struct meta_block*)BLOCK_HEAD;
 
+    while (curr != NULL) {
+        if (curr->vacant == 1 && size <= curr->size) {
+
+            return sizeof(struct meta_block) + (void *)curr;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    // Create new block
     void* address = sbrk(size + sizeof(struct meta_block));
     if (address == (void *) -1) { return NULL; } // sbrk has failed
 
@@ -22,10 +36,35 @@ void* my_malloc(size_t size) {
     meta_pointer->vacant = 0;
     meta_pointer->next = NULL;
 
-    return address + sizeof(struct meta_block);
+    void* output = (void*)meta_pointer;
+
+    prev->next = output;
+
+    return output + sizeof(struct meta_block);
+}
+
+void* my_malloc(size_t size) {
+    if (size == 0) { return NULL; }
+    if (BLOCK_HEAD == NULL) {
+        // Initialize linked list
+        BLOCK_HEAD = sbrk(0); 
+
+        void* address = sbrk(size + sizeof(struct meta_block));
+        if (address == (void *) -1) { return NULL; } // sbrk has failed
+
+        struct meta_block *meta_pointer = (struct meta_block *)address;
+        meta_pointer->size = size;
+        meta_pointer->vacant = 0;
+        meta_pointer->next = NULL;
+
+        return BLOCK_HEAD + sizeof(struct meta_block);
+    }
+
+    void* address = find_block(size);
+    return address;
 }
 
 void my_free(void* ptr) {
-    struct meta_block *meta_pointer = (struct meta_block *)ptr;
+    struct meta_block *meta_pointer = (struct meta_block *)(ptr - sizeof(struct meta_block));
     meta_pointer->vacant = 1;
 }
